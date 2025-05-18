7737b81c9742057c8d6910cccfc1f362a7f2597c
Sanjana Baragi <152136888+Sanjanabaragi@users.noreply.github.com>
2025-05-18 12:20:26 +0530

    uploaded src files

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import ResumeAnalyzer from "./pages/ResumeAnalyzer";
+import CareerTest from "./pages/CareerTest";
+import Recommendations from "./pages/Recommendations";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/resume-builder" element={<ResumeBuilder />} />
+          <Route path="/resume-analyzer" element={<ResumeAnalyzer />} />
+          <Route path="/career-test" element={<CareerTest />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
